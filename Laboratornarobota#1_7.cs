dbb7c43eba68efc5d2472b4792bf56c5cdc4bbbe
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-09-23 18:13:36 +0300

    Add files via upload

---
+++ Laboratornarobota#1_7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_1_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть температуру в градусах Цельсія: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+        }
+    }
+}
