c7edbceedf5f3959e043ba23644fff22d6df5d58
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-02 14:28:43 +0300

    Add files via upload

---
+++ Laboratornarobota#2_7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_2_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть число:");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число парне");
+            }
+            else
+            {
+                Console.WriteLine("Число непарне");
+            }
+             if (number % 3 == 0)
+            {
+                Console.WriteLine("Число кратне трьом");
+            }
+             else
+            {
+                Console.WriteLine("Число не кратне трьом");
+            }
+        }
+    }
+}
