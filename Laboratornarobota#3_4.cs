9703338b2c5dafba70db1539f11c706b7a67d803
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-03 18:52:09 +0300

    Add files via upload

---
+++ Laboratornarobota#3_4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            int i=number;
+            do
+            {
+                factorial = factorial * i;
+                --i;
+            }
+            while (i > 1);
+
+            Console.WriteLine($"{number}! = {factorial}");
+
+            Console.ReadLine();
+        }
+    }
+}
