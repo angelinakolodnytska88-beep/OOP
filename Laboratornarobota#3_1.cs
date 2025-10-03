22c70cb96b7bec1d6b7dd1e9b58911f13f4cc4d4
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-03 17:54:35 +0300

    Add files via upload

---
+++ Laboratornarobota#3_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 2;
+            while (n < 21)
+            {
+                Console.WriteLine(n);
+                n += 2;
+            }
+        }
+    }
+}
---
+++ Laboratornarobota#3_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Сума чисел від 1 до 100: {sum}");
+        }
+    }
+}
