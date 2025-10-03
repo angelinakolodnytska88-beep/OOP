0c8d4f7491c4c939250c96b6456a1a14629b3108
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-03 18:52:26 +0300

    Add files via upload

---
+++ Laboratornarobota#3_3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Введіть число:");
+                    int i = 1;
+                    int number = Convert.ToInt32(Console.ReadLine());
+            while (i <= 10)
+                    {
+                        int result = number * i;
+                        Console.WriteLine($"{number} × {i} = {result}");
+                        i++;
+                    }
+            Console.ReadLine();
+        }
+    }
+}
