d7ea4f0d261acb5b94c713467ae92a20d47c2fe2
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-08 11:52:59 +0300

    Add files via upload

---
+++ Laboratornarobota#3_6.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Laboratornarobota_3_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть число:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            string binary = "";
+            bool isNegative = false;
+
+            if (num < 0)
+            {
+                isNegative = true;
+                num = Math.Abs(num);
+            }
+
+            while (num > 0)
+            {
+                binary = (num % 2) + binary;
+                num /= 2;
+            }
+
+            if (isNegative)
+            {
+                binary = "-" + binary;
+            }
+
+            Console.WriteLine("Двійкове представлення: " + binary);
+        }
+    }
+}
