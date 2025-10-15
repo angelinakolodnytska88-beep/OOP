e75939eafc37a4282a96a8a5046dc711a9ddcf78
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-15 12:20:49 +0300

    Add files via upload

---
+++ Laboratornarobota#4_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 5, 6, 8 };
+            int sum = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+                sum += myArray[i];
+            }
+            double average = (double)sum / myArray.Length;
+            Console.WriteLine("Середнє значення: " + average);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratornarobota#4_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]Array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Array elements:");
+            bool isPalindrome = true;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(Array[i] + " ");
+                if (Array[i] != Array[Array.Length - 1 - i])
+                {
+                    isPalindrome = false;
+                }
+             }
+            Console.WriteLine();
+            if (isPalindrome)
+                Console.WriteLine("Результат: Масив Є паліндромом");
+            else
+                Console.WriteLine("Результат: Масив НЕ є паліндромом");
+        }
+
+    }
+}
