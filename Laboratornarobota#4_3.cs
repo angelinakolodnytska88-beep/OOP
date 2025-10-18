0e291b6a811a9d58ed49ec53eed773b3c7db518c
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-18 14:53:42 +0300

    Add files via upload

---
+++ Laboratornarobota#4_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_4_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Array elements:");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(Array[i] + " ");
+            }
+            bool foundEven = false;
+            Console.Write("Парні елементи: ");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] % 2 == 0)
+                {
+                    if (foundEven)
+                        Console.Write(", ");
+                    Console.Write(Array[i]);
+                    foundEven = true;
+                }
+            }
+            if (!foundEven)
+                Console.Write("відсутні");
+            Console.WriteLine();
+        }
+    }
+}
