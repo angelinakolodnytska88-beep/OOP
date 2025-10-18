1674bc67d537c585967249ac5692195604206e1a
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-18 15:06:24 +0300

    Add files via upload

---
+++ Laboratornarobota#4_4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Array = { -5, -3, 7, 3, -9, -3, 1 };
+            Console.Write("Масив: ");
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(Array[i]);
+                if (i < Array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+            int positiveSum = 0;
+            foreach (int number in Array)
+            {
+                if (number > 0)
+                {
+                    positiveSum += number;
+                }
+            }
+            Console.WriteLine("Sum of positive elements: " + positiveSum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Laboratornarobota#4_5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 5, 3, 7, 3, 9, 3, 1 };
+            Console.Write("Масив: ");
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(Array[i]);
+                if (i < Array.Length - 1) 
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+
+            int El = 3;
+            int index = Array.ToList().IndexOf(El);
+            if (index != -1)
+            {
+                Console.WriteLine($"Елемент {El} знайдено на позиції {index}");
+            }
+            else
+            {
+                Console.WriteLine($"Елемент {El} не знайдено в масиві.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
