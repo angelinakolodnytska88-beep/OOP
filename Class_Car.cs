4fbc77e6f86af4c2bec386845b1528f262a89823
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-29 11:59:38 +0200

    Add files via upload

---
+++ Class_Car.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace CarApp
+{
+    class Car
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+        public const string CompanyName = "AutoCompany";
+
+        public Car()
+        {
+            Name = "NoName";
+            Color = "NoColor";
+            Price = 0;
+        }
+
+        public Car(string name, string color, double price)
+        {
+            Name = name;
+            Color = color;
+            Price = price;
+        }
+
+        public void Input()
+        {
+            Console.Write("Введіть назву авто: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Введіть колір авто: ");
+            Color = Console.ReadLine();
+
+            Console.Write("Введіть ціну авто: ");
+            Price = double.Parse(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Компанія: " + CompanyName);
+            Console.WriteLine("Назва: " + Name);
+            Console.WriteLine("Колір: " + Color);
+            Console.WriteLine("Ціна: " + Price + " грн");
+            Console.WriteLine();
+        }
+
+        public void ChangePrice(double x)
+        {
+            Price = Price - Price * x / 100;
+        }
+
+        public string PrintInfo()
+        {
+            return CompanyName + " " + Name + ", колір: " + Color + ", ціна: " + Price + " грн";
+        }
+
+        static void Main()
+        {
+            Car[] cars = new Car[3];
+
+            Console.WriteLine("Введення даних про 3 авто");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("\nАвто #" + (i + 1));
+                cars[i] = new Car();
+                cars[i].Input();
+            }
+
+            Console.WriteLine("\nЗменшення ціни на 10%");
+            for (int i = 0; i < 3; i++)
+            {
+                cars[i].ChangePrice(10);
+                cars[i].Print();
+            }
+
+            Console.Write("\nВведіть новий колір для авто білого кольору: ");
+            string newColor = Console.ReadLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (cars[i].Color.ToLower() == "білий" || cars[i].Color.ToLower() == "white")
+                {
+                    cars[i].Color = newColor;
+                }
+            }
+
+            Console.WriteLine("\nДані про авто після перекрашування");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(cars[i].PrintInfo());
+            }
+        }
+    }
+}
+
---
+++ Class_Person_.cs
@@ -0,0 +1,106 @@
+﻿using System;
+namespace PersonApp
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthYear;
+        public string Name
+        {
+            get { return name; }
+        }
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+        }
+        public Person()
+        {
+            name = "NoName";
+            birthYear = DateTime.Now;
+        }
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+        public int Age()
+        {
+            return DateTime.Now.Year - birthYear.Year;
+        }
+
+        public void Input()
+        {
+            Console.Write("Введіть ім'я: ");
+            name = Console.ReadLine();
+            Console.Write("Введіть рік народження: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Введіть місяць народження (1-12): ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("Введіть день народження (1-31): ");
+            int day = int.Parse(Console.ReadLine());
+            birthYear = new DateTime(year, month, day);
+        }
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+        public string ToString()
+        {
+            return $"Ім'я: {name}, Дата народження: {birthYear.ToShortDateString()}, Вік: {Age()}";
+        }
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+        public static bool operator ==(Person p1, Person p2)
+        {
+            return p1.name == p2.name;
+        }
+        public static bool operator !=(Person p1, Person p2)
+        {
+            return !(p1 == p2);
+        }
+        static void Main()
+        {
+            Person[] people = new Person[6];
+            Console.WriteLine("Введіть дані про 6 осіб:\n");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Особа #{i + 1}:");
+                people[i] = new Person();
+                people[i].Input();
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nІм'я та вік кожної особи");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"{people[i].Name} - {people[i].Age()} років");
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                if (people[i].Age() < 16)
+                {
+                    people[i].ChangeName("Very Young");
+                }
+            }
+            Console.WriteLine("\nІнформація про всіх людей");
+            for (int i = 0; i < 6; i++)
+            {
+                people[i].Output();
+            }
+            Console.WriteLine("\nЛюди з однаковими іменами");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = i + 1; j < 6; j++)
+                {
+                    if (people[i] == people[j])
+                    {
+                        Console.WriteLine($"\nОсоба #{i + 1} та Особа #{j + 1} мають однакові імена:");
+                        people[i].Output();
+                        people[j].Output();
+                    }
+                }
+            }
+        }
+    }
+}
