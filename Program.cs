6532efc352ef2f9a9bf81cd3d8cba76b8dd8f084
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-10-29 16:41:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornarobota_7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
