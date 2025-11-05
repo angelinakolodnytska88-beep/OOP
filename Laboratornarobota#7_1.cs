64edbf183fc9b54ce38ea88b8f7b795e8b597416
ANHELINA_KOLODNYTSKA <angelinakolodnytska88@gmail.com>
2025-11-05 10:40:10 +0200

    Add files via upload

---
+++ Laboratornarobota#7_1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Laboratornarobota_7_1_
+{
+    abstract class Figure
+    {
+        public abstract string GetName();
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+
+    class Circle : Figure
+    {
+        double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        public override string GetName()
+        {
+            return "Коло";
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+
+    class Square : Figure
+    {
+        double width;
+        double height;
+
+        public Square(double w, double h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public override string GetName()
+        {
+            return "Прямокутник";
+        }
+
+        public override double CalculateArea()
+        {
+            return width * height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (width + height);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure circle = new Circle(5);
+            Figure square = new Square(4, 6);
+
+            Console.WriteLine($"{circle.GetName()}: Площа = {circle.CalculateArea()}, Периметр= {circle.CalculatePerimeter()}");
+            Console.WriteLine($"{square.GetName()}: Площа = {square.CalculateArea()}, Периметр = {square.CalculatePerimeter()}");
+        }
+    }
+}
---
+++ Laboratornarobota#7_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Fruit   
+{
+    public string name = "Це фрукт";
+    
+}
+
+class Apple : Fruit{
+    public new string name = "Яблуко";
+    public string Variety = "Голден";  
+    public string Color = "Жовте";  
+}
+class Peart : Fruit{
+    public new string name = "Груша";
+    public string Variety = "Зимна";  
+    public string Color = "Зелена";  
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Apple apple = new Apple();
+        Console.WriteLine($"{apple.name}: {apple.Variety}, {apple.Color}");
+
+        Peart pear = new Peart();
+        Console.WriteLine($"{pear.name}: {pear.Variety}, {pear.Color}");
+    }
+}
